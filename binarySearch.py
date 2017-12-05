819bd88eabcc828d564f4791509e1bb79547d12f
salonwrap <31446902+salonwrap@users.noreply.github.com>
2017-12-05 14:53:28 -0800

    Add files via upload

---
+++ binarySearch.py
@@ -0,0 +1,27 @@
+def binarySearch(alist,item):
+	if len(alist) == 0:
+		print('item not found')
+		return False
+	else: 
+		midpoint = len(alist)//2
+		print(len(alist))
+		print('lenght of list')
+		print(alist[midpoint])
+		print('midpoint value')
+		if alist[midpoint] == item:
+			return True
+		else:
+			if item<alist[midpoint]:
+				print('item is less than midpoint')
+				print(alist[:midpoint])
+				return binarySearch(alist[:midpoint], item)
+			else:
+				print('item is greater than midpoint')
+				print(alist[midpoint +1:])
+				return binarySearch(alist[midpoint + 1:], item)
+
+
+
+testlist = [1,3,4,6,7,10]
+#print(binarySearch(testlist,3))
+print(binarySearch(testlist,2))
